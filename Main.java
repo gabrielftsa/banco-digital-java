578b22ad614f011f54f62b96d3fbc3a67243dcd1
Gabriel Feitosa <70492667+gabrielftsa@users.noreply.github.com>
2023-02-21 06:52:48 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-
-
         var cliente1 = new Cliente("Gabriel Feitosa");
         var cliente2 = new Cliente("Daniel Marcelo da Silva");
         var cliente3 = new Cliente("Julia Silva");
-
 
         var contaCorrente1 = new ContaCorrente(cliente1);
         var contaPoupanca2 = new ContaPoupanca(cliente2);
